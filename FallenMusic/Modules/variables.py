81ddc8bec7ec7649f421686d5ac6ef6ac49c6833
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-03-07 02:29:47 +0400

    update variables.py

--- FallenMusic/Modules/variables.py
+++ FallenMusic/Modules/variables.py
@@ -15,20 +15,20 @@ async def get_vars(_, message: Message):
             chat_id=int(config.OWNER_ID),
             text=f"""<u>**{BOT_NAME} Dəyişənləri konfiqurasiya edin :**</u>
 
-**ᴀᴘɪ_ɪᴅ :** `{config.API_ID}`
-**ᴀᴘɪ_ʜᴀsʜ :** `{config.API_HASH}`
+**ᴀᴘɪ_ɪᴅ:** `{config.API_ID}`
+**ᴀᴘɪ_ʜᴀsʜ:** `{config.API_HASH}`
 
-**ʙᴏᴛ_ᴛᴏᴋᴇɴ :** `{config.BOT_TOKEN}`
-**ᴅᴜʀᴀᴛɪᴏɴ_ʟɪᴍɪᴛ :** `{config.DURATION_LIMIT}`
+**ʙᴏᴛ_ᴛᴏᴋᴇɴ:** `{config.BOT_TOKEN}`
+**ᴅᴜʀᴀᴛɪᴏɴ_ʟɪᴍɪᴛ:** `{config.DURATION_LIMIT}`
 
-**ᴏᴡɴᴇʀ_ɪᴅ :** `{config.OWNER_ID}`
-**sᴜᴅᴏ_ᴜsᴇʀs :** `{config.SUDO_USERS}`
+**ᴏᴡɴᴇʀ_ɪᴅ:** `{config.OWNER_ID}`
+**sᴜᴅᴏ_ᴜsᴇʀs:** `{config.SUDO_USERS}`
 
-**ᴘɪɴɢ_ɪᴍɢ :** `{config.PING_IMG}`
-**sᴛᴀʀᴛ_ɪᴍɢ :** `{config.START_IMG}`
-**sᴜᴘᴘᴏʀᴛ_ᴄʜᴀᴛ :** `{config.SUPPORT_CHAT}`
+**ᴘɪɴɢ_ɪᴍɢ:** `{config.PING_IMG}`
+**sᴛᴀʀᴛ_ɪᴍɢ:** `{config.START_IMG}`
+**sᴜᴘᴘᴏʀᴛ_ᴄʜᴀᴛ:** `{config.SUPPORT_CHAT}`
 
-**sᴇssɪᴏɴ :** `{config.SESSION}`""",
+**sᴇssɪᴏɴ:** `{config.SESSION}`""",
             disable_web_page_preview=True,
         )
     except:
