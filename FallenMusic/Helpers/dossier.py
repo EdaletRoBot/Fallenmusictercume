ed55812322b9c2e9a9c8d38f576145bd07841571
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-03-07 05:38:32 +0400

    update dossier.py

--- FallenMusic/Helpers/dossier.py
+++ FallenMusic/Helpers/dossier.py
@@ -20,15 +20,15 @@ HELP_TEXT = f"""
 <u>❄ **İstifadəçilər üçün mövcud əmrlər {BOT_NAME} :**</u>
 
 •━━━━━━━━•••━━━━━━━━•
-๏ /play - Video çatda tələb olunan treki yayımlayan ulduzlar.
-๏ /pause - Cari ifa axını dayandırın.
-๏ /resume - Dayandırılmış yayımı davam etdirin.
-๏ /skip - Cari ifa axınını atlayın və növbədə olan növbəti treki yayımlamağa başlayın.
-๏ /end - Növbəni təmizləyir və cari ifa axınını bitirir.
-๏ /ping - Botun ping və sistem statistikasını göstərin.
-๏ /sudolist - Botun sodo istifadəçilərinin siyahısını göstərin.
-๏ /song - İstədiyiniz mahnını yükləyin və sizə göndərin.
-๏ /search - Verilən sorğunu youtube-da axtarır və nəticəni sizə göstərir.
+/play - Video çatda tələb olunan treki yayımlayan ulduzlar.
+/pause - Cari ifa axını dayandırın.
+/resume - Dayandırılmış yayımı davam etdirin.
+/skip - Cari ifa axınını atlayın və növbədə olan növbəti treki yayımlamağa başlayın.
+/end - Növbəni təmizləyir və cari ifa axınını bitirir.
+/ping - Botun ping və sistem statistikasını göstərin.
+/sudolist - Botun sodo istifadəçilərinin siyahısını göstərin.
+/song - İstədiyiniz mahnını yükləyin və sizə göndərin.
+/search - Verilən sorğunu youtube-da axtarır və nəticəni sizə göstərir.
 •━━━━━━━━•••━━━━━━━━•
 """
 
@@ -36,14 +36,14 @@ HELP_SUDO = f"""
 <u>✨ **Sudo əmrləri daxil olur {BOT_NAME} :**</u>
 
 •━━━━━━━━•••━━━━━━━━•
-๏ /activevc - Hazırda aktiv səsli söhbətlərin siyahısını göstərir.
-๏ /eval və ya /sh - Verilmiş kodu bot terminalında işlədir.
-๏ /speedtest - Botlar serverində sürət testini həyata keçirir.
-๏ /sysstats - Botlar serverinin sistem statistikasını göstərir.
-๏ /setname [Mətn və ya mətnə cavab] - Köməkçi hesabının adını dəyişdirin.
-๏ /setbio [Mətn və ya mətnə cavab] - Köməkçi hesabının bio-nu dəyişdirin.
-๏ /setpfp [Fotoya cavab verin] - Köməkçi hesabının pfp-ni dəyişdirin.
-๏ /delpfp - Köməkçi hesabının cari pfp-ni silin.
+/activevc - Hazırda aktiv səsli söhbətlərin siyahısını göstərir.
+/eval və ya /sh - Verilmiş kodu bot terminalında işlədir.
+/speedtest - Botlar serverində sürət testini həyata keçirir.
+/sysstats - Botlar serverinin sistem statistikasını göstərir.
+/setname [Mətn və ya mətnə cavab] - Köməkçi hesabının adını dəyişdirin.
+/setbio [Mətn və ya mətnə cavab] - Köməkçi hesabının bio-nu dəyişdirin.
+/setpfp [Fotoya cavab verin] - Köməkçi hesabının pfp-ni dəyişdirin.
+/delpfp - Köməkçi hesabının cari pfp-ni silin.
 •━━━━━━━━•••━━━━━━━━•
 """
 
@@ -51,11 +51,11 @@ HELP_DEV = f"""
 <u>✨ **Sahib əmrləri {BOT_NAME} :**</u>
 
 •━━━━━━━━•••━━━━━━━━•
-๏ /config - Botun bütün konfiqurasiya dəyişənlərini əldə etmək üçün.
-๏ /broadcast [Mesaj göndərin və ya mesaja cavab verin] - Mesajı botun xidmət edilən çatlarına çatdırın.
-๏ /rmdownloads - Botlar serverində yüklənmiş keş fayllarını təmizləyir.
-๏ /leaveall - Köməkçi hesabına bütün söhbətləri tərk etməyi əmr edir.
-๏ /addsudo [İstifadəçi adı və ya istifadəçiyə cavab] - İstifadəçini sudo users lisr-ə əlavə edin.
-๏ /rmsudo [İstifadəçi adı və ya istifadəçiyə cavab] - İstifadəçini sudo istifadəçiləri siyahısından çıxarın.
+/config - Botun bütün konfiqurasiya dəyişənlərini əldə etmək üçün.
+/broadcast [Mesaj göndərin və ya mesaja cavab verin] - Mesajı botun xidmət edilən çatlarına çatdırın.
+/rmdownloads - Botlar serverində yüklənmiş keş fayllarını təmizləyir.
+/leaveall - Köməkçi hesabına bütün söhbətləri tərk etməyi əmr edir.
+/addsudo [İstifadəçi adı və ya istifadəçiyə cavab] - İstifadəçini sudo users lisr-ə əlavə edin.
+/rmsudo [İstifadəçi adı və ya istifadəçiyə cavab] - İstifadəçini sudo istifadəçiləri siyahısından çıxarın.
 •━━━━━━━━•••━━━━━━━━•
 """
