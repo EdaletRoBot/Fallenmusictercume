0d38da393d6d252a550902d7984e2b45c8472e75
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-03-07 17:39:47 +0400

    update resume.py

--- FallenMusic/Modules/resume.py
+++ FallenMusic/Modules/resume.py
@@ -14,7 +14,7 @@ async def res_str(_, message: Message):
         pass
 
     if await is_streaming(message.chat.id):
-        return await message.reply_text("Saxlanıldı")
+        return await message.reply_text("Yadda saxlamısınız ki, yayımı dayandırdınız ?")
     await stream_on(message.chat.id)
     await pytgcalls.resume_stream(message.chat.id)
     return await message.reply_text(
