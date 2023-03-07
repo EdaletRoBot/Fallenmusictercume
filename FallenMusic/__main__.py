5e493f65cb6e4c881323fb389a7b8a9efe91cf9d
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-03-07 04:28:56 +0400

    update __main__.py

--- FallenMusic/__main__.py
+++ FallenMusic/__main__.py
@@ -36,7 +36,7 @@ async def fallen_startup():
     try:
         await app.send_message(
             SUNAME,
-            f"✯ RaviVeyi Music Bot ✯\n\n𖢵 ID : `{BOT_ID}`\n𖢵 Ad : {BOT_NAME}\n𖢵 istifadəçi adı : @{BOT_USERNAME}",
+            f"Rahid Music ♫︎\n\n𖢵 ID: `{BOT_ID}`\n𖢵 Ad: {BOT_NAME}\n𖢵 istifadəçi adı: @{BOT_USERNAME}",
         )
     except:
         LOGGER.error(
@@ -46,7 +46,7 @@ async def fallen_startup():
     try:
         await app2.send_message(
             SUNAME,
-            f"✯ RaviVeyi Music Bot Assistent ✯\n\n𖢵 ID : `{ASS_ID}`\n𖢵 Ad : {ASS_NAME}\n𖢵 istifadəçi adı : @{ASS_USERNAME}",
+            f"✯ Rahid Music Bot Assistent ✯\n\n𖢵 ID: `{ASS_ID}`\n𖢵 Ad: {ASS_NAME}\n𖢵 istifadəçi adı: @{ASS_USERNAME}",
         )
     except:
         LOGGER.error(
@@ -67,4 +67,4 @@ async def fallen_startup():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(fallen_startup())
-    LOGGER.error("RaviVeyi Musiqi Botu Dayandı.")
+    LOGGER.error("Rahis musiqi botu dayandı.")
