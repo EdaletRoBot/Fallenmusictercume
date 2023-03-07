f800ae3e5fda80ae25f79ab2c2e81f1646e55c37
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-03-08 01:49:57 +0400

    update song.py

--- FallenMusic/Modules/song.py
+++ FallenMusic/Modules/song.py
@@ -16,7 +16,7 @@ async def song(_, message: Message):
         await message.delete()
     except:
         pass
-    m = await message.reply_text("🔍")
+    m = await message.reply_text("**🔍 Göndərirəm...**")
 
     query = "".join(" " + str(i) for i in message.command[1:])
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
@@ -36,13 +36,13 @@ async def song(_, message: Message):
             f"youtube-dl-dən treki əldə etmək alınmadı.\n\n**səbəb :** `{ex}`"
         )
 
-    await m.edit_text("🔍 **Musiqi axtarılır, zəhmət olmasa gözləyin...**")
+    await m.edit_text("**🔍 Musiqi axtarılır, zəhmət olmasa gözləyin...**")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎵 **Başlıq:** [{title[:23]}]({link})\n⏳ **Müddət:** `{duration}`\n🤖 Bot:** {BOT_MENTION}"
+        rep = f"🎵 **Başlıq:** [{title[:23]}]({link})\n⏳ **Müddət:** `{duration}`\n🤖 **Bot:** {BOT_MENTION}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
