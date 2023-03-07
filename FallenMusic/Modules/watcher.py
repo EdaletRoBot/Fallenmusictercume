b9171002142fabb398a0abe8a2b06655051e3b36
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-03-07 16:54:33 +0400

    update watcher.py

--- FallenMusic/Modules/watcher.py
+++ FallenMusic/Modules/watcher.py
@@ -57,7 +57,7 @@ async def on_stream_end(pytgcalls, update: Update):
     else:
         process = await app.send_message(
             chat_id=chat_id,
-            text="⏭️ Növbəti trek endirilir...",
+            text="⏭️ Növbəti musiqi oxunur...",
         )
         title = get[0]["title"]
         duration = get[0]["duration"]
@@ -83,6 +83,6 @@ async def on_stream_end(pytgcalls, update: Update):
         await app.send_photo(
             chat_id=chat_id,
             photo=img,
-            caption=f"🎵 **Başlıq:** [{title[:27]}](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n⏳ **Müddət:** `{duration}`\n👤 **Tələb:** {req_by}",
+            caption=f"🎵 **Başlıq:** [{title[:27]}](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n⏳ **Müddət:** `{duration}`\n👤 **Tələt:** {req_by}",
             reply_markup=buttons,
         )
