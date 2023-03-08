f912effc330e8dc33e75463900f00824cf70b756
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-03-08 13:36:53 +0400

    update assistant.py

--- FallenMusic/Modules/assistant.py
+++ FallenMusic/Modules/assistant.py
@@ -7,18 +7,18 @@ from FallenMusic import ASS_MENTION, LOGGER, SUDOERS, app, app2
 @app.on_message(filters.command(["asspfp", "setpfp"]) & SUDOERS)
 async def set_pfp(_, message: Message):
     if message.reply_to_message.photo:
-        fuk = await message.reply_text("» Köməkçinin profil şəklinin dəyişdirilməsi...")
+        fuk = await message.reply_text("Köməkçinin profil şəklinin dəyişdirilməsi...")
         img = await message.reply_to_message.download()
         try:
             await app2.set_profile_photo(photo=img)
             return await fuk.edit_text(
-                f"» {ASS_MENTION} Profil şəkli uğurla dəyişdirildi."
+                f"{ASS_MENTION} Profil şəkli uğurla dəyişdirildi."
             )
         except:
-            return await fuk.edit_text("» Assistentin profil şəklini dəyişmək alınmadı.")
+            return await fuk.edit_text("Assistant profil şəklini dəyişmək alınmadı.")
     else:
         await message.reply_text(
-            "» Köməkçinin profil şəklini dəyişmək üçün fotoya cavab verin."
+            "Köməkçinin profil şəklini dəyişmək üçün fotoya cavab verin."
         )
 
 
@@ -28,11 +28,11 @@ async def set_pfp(_, message: Message):
         pfp = [p async for p in app2.get_chat_photos("me")]
         await app2.delete_profile_photos(pfp[0].file_id)
         return await message.reply_text(
-            "» Köməkçinin profil şəkli uğurla silindi."
+            "Köməkçinin profil şəkli uğurla silindi."
         )
     except Exception as ex:
         LOGGER.error(ex)
-        await message.reply_text("» Assistentin profil şəklini silmək alınmadı.")
+        await message.reply_text("Assistantın profil şəklini silmək alınmadı.")
 
 
 @app.on_message(filters.command(["assbio", "setbio"]) & SUDOERS)
@@ -43,7 +43,7 @@ async def set_bio(_, message: Message):
             newbio = msg.text
             await app2.update_profile(bio=newbio)
             return await message.reply_text(
-                f"» {ASS_MENTION} Bio uğurla dəyişdi."
+                f"{ASS_MENTION} Bio uğurla dəyişdi."
             )
     elif len(message.command) != 1:
         newbio = message.text.split(None, 1)[1]
@@ -51,7 +51,7 @@ async def set_bio(_, message: Message):
         return await message.reply_text(f"» {ASS_MENTION} Bio uğurla dəyişdi.")
     else:
         return await message.reply_text(
-            "» mesaja cavab verin və ya onu köməkçinin tərcümeyi-halı kimi təyin etmək üçün mətn verin."
+            "Mesaja cavab verin və ya onu köməkçinin tərcümeyi-halı kimi təyin etmək üçün mətn verin."
         )
 
 
@@ -63,13 +63,13 @@ async def set_name(_, message: Message):
             name = msg.text
             await app2.update_profile(first_name=name)
             return await message.reply_text(
-                f"» {ASS_MENTION} Ad uğurla dəyişdi."
+                f"{ASS_MENTION} Adı uğurla dəyişdi."
             )
     elif len(message.command) != 1:
         name = message.text.split(None, 1)[1]
         await app2.update_profile(first_name=name, last_name="")
-        return await message.reply_text(f"» {ASS_MENTION} Ad uğurla dəyişdi.")
+        return await message.reply_text(f"{ASS_MENTION} Adı uğurla dəyişdi.")
     else:
         return await message.reply_text(
-            "» mesaja cavab verin və ya onu köməkçinin yeni adı kimi təyin etmək üçün mətn verin."
+            "Mesaja cavab verin və ya onu köməkçinin yeni adı kimi təyin etmək üçün mətn verin."
         )
