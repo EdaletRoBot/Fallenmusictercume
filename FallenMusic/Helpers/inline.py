8347449395be7d2e4f5589bfa3b4354be03077c2
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-03-30 04:29:36 +0400

    update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -34,7 +34,7 @@ pm_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="✅ Rəsmi Kanal", url="https://t.me/Rahid_44"
+            text="✅ Rəsmi Kanal", url="https://t.me/Sohbet_Region"
         ),
         InlineKeyboardButton(text="👨🏻‍💻 Bot Sahibi", user_id=config.OWNER_ID),
     ],
@@ -54,7 +54,7 @@ gp_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="✅ Rəsmi Kanal", url="hhttps://t.me/Rahid_44"
+            text="✅ Rəsmi Kanal", url="hhttps://t.me/Sohbet_Region"
         ),
         InlineKeyboardButton(text="👨🏻‍💻 Bot Sahibi", user_id=config.OWNER_ID),
     ],
