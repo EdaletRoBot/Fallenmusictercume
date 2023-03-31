86d28e40ca51bfc409978ff8138ce5547d37e7a4
IlkinNehmedov <129421257+IlkinNehmedov@users.noreply.github.com>
2023-03-31 19:05:13 +0400

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ START_IMG = getenv("START_IMG", "https://telegra.ph/file/d008e81582bfd871ea9a0.j
 
 SESSION = getenv("SESSION", "AQA8iTM3F3n7c0CSXq_1fUAv6XT3w1zciOxbhQGwxXu185gV_hBtj9Wh09OAWsB1jAJ33Him6CqGmNJD5mMuTVVyrLRzz_d8SmIO8LnNPS9-6HKAOV2DHZ3BZbd1913wUVVtCV8dkqU2QbPlfuTsBvf_otFevLoLSN3UyMfEc7gHqd4knN534iBJJZRWb_xVHauO2FwjgiXBafTm22JUDmtmB7gPepY8USbRTlaEIGPP2ZwzhJrT9r_cNVjlESqL_--sT0_KDi-NO72lgJUcBVta2SKbt7N7pgLRVv4Zem7RfxI9M3rTwBlBdHXXbuqqHpd2PchrMt9IG3YtvXHXt3SJAAAAAWGaX7EA")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sohbet_Region")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Sohbet_Region")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ilkinsupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ilkinsupport")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6045596584").split()))
 
