a29eaaee8bbcc07e555c0a950404a2edf8815544
Ədalət <99437747+EdaletRoBot@users.noreply.github.com>
2023-04-27 12:56:32 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID", "5540993505"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/3b49e67c2e79d73923608.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/3b49e67c2e79d73923608.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/9bbe79f7904d8e30053d7.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/9bbe79f7904d8e30053d7.jpg")
 
 SESSION = getenv("SESSION", "AQCaZ9HbvXp2U54PviJgsPjROttd2bIejwyXlC1zcHN7qNBZZa5SaIUqgoTPM2lFEc_M0vvdVTuHL8Mogxs8c_bsHkKdNh4TgufkuAyJdYTdfkohotgyp-O4ELzW2_Vs7ievvUtoifxhcV5JwqV330vAuwoabEbaIl-3_FAYZmqWwVk6ah9lkE-5BoNd9hIx6IyhWlnm50JymSUUvb89RZfKmEQ4f6Mubood40PvNps9QUSn39ZOE9MxUmaoXE9dVyP-59EgCKZM_fjSF1tupK6kFPioDiLNxtuzyQyIq2Q5-lPDfJOEq-zL6aNR31spEa0-Z3IrtLCEkjsWW__H_QigAAAAAUffa1cA")
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/edaletproject")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5540993505").split()))
 
 
-FAILED = "https://telegra.ph/file/3b49e67c2e79d73923608.jpg"
+FAILED = "https://telegra.ph/file/9bbe79f7904d8e30053d7.jpg"
