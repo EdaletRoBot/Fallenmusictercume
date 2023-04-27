1d4da58a89a6d6a7d3b2f5a3da4295d79794b128
Ədalət <99437747+EdaletRoBot@users.noreply.github.com>
2023-04-27 12:58:33 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", "21257388"))
-API_HASH = getenv("API_HASH", "26418a3017c92dca31d8b973f1f81018")
+API_ID = int(getenv("API_ID", "23684473"))
+API_HASH = getenv("API_HASH", "a2161b18cd38679ed40beed0d82f0b88")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5786085295:AAGxg4v-kCZbS0pB9wxy_dX2hKT1AZzyOCU")
+BOT_TOKEN = getenv("BOT_TOKEN", "6119118831:AAEK2OsoMs-rb1oaSopzvvg-YlY1Hij7pDE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID", "5540993505"))
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID", "5540993505"))
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/9bbe79f7904d8e30053d7.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/9bbe79f7904d8e30053d7.jpg")
 
-SESSION = getenv("SESSION", "AQCaZ9HbvXp2U54PviJgsPjROttd2bIejwyXlC1zcHN7qNBZZa5SaIUqgoTPM2lFEc_M0vvdVTuHL8Mogxs8c_bsHkKdNh4TgufkuAyJdYTdfkohotgyp-O4ELzW2_Vs7ievvUtoifxhcV5JwqV330vAuwoabEbaIl-3_FAYZmqWwVk6ah9lkE-5BoNd9hIx6IyhWlnm50JymSUUvb89RZfKmEQ4f6Mubood40PvNps9QUSn39ZOE9MxUmaoXE9dVyP-59EgCKZM_fjSF1tupK6kFPioDiLNxtuzyQyIq2Q5-lPDfJOEq-zL6aNR31spEa0-Z3IrtLCEkjsWW__H_QigAAAAAUffa1cA")
+SESSION = getenv("SESSION", "AQCaZ9HbvXp2U54PviJgsPjROttd2bIejwyXlC1zcAgCnqOEw6ZCEe7P8kySWLo1NmhvXJqXPF1Bb0uses2PfWY9SM3pVZHwhgfVBlMUVmO9ZuwmiYOW7GVLMo6VsOFAJ6HIIt1HE0APGe7Cxj7vfg3bcEAfWK5DwFAomdsDEjonPGxOmVaP83HwMAtZWsR_93ArPQHB7NnHecVaNQXGYHCqqV8-APHRh_YJpfUd3SoCVTtDhrrzfkpL6E-mowQLMzggSNuDwsdzua-rJwX-9gc2s2IZimdd33dQPwW-7KYiXTGtt8IJhoIuN5kFTfjeBH549rwTN4MYOJsXyE0FNe4255wdgDcv7Lb6j4Sk6TZtmvFuL_jTi7Blq8qJcdFzJAAAAAVve29YA")
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/edaletsup")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/edaletproject")
